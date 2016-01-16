990b87335486389a480cd31a779807a537ab0138
Hironobu Nishikokura <nishiko@mindia.jp>
2016-01-17 08:01:55 +0900

    gmailのパーマリンク追加

--- lib/kintone_sync/gmail.rb
+++ lib/kintone_sync/gmail.rb
@@ -23,12 +23,24 @@ module KintoneSync
       @gmails.each do |gmail|
         gmail.inbox.find.each do |mail|
           params = {}
-          #raise mail.charset.inspect # => 'UTF-8'
-          params[:from] = ::Kconv.toutf8(mail.header['from'].value)
-          params[:to] = ::Kconv.toutf8(mail.header['to'].value)
+          params[:account] = gmail.username
+          
+          %w(from to).each do |key|
+            params[key.to_sym] = ::Kconv.toutf8(mail.header[key].value)
+          end
+          #params[:sender] = ::Kconv.toutf8(mail.header['sender'].value)
+          #params[:cc] = ::Kconv.toutf8(mail.header['cc'].value)
+
           params[:subject] = ::Kconv.toutf8(mail.subject)
           #params[:body] = Sanitize.fragment(Kconv.toutf8(mail.body.to_s))
-          params[:body] = Sanitize.fragment(mail.body.decoded)
+          #params[:body] = Sanitize.fragment(mail.body.decoded)
+          body = (mail.parts.present? ? mail.parts.first : mail.body).decoded
+          params[:body] =  body
+          params[:created_at] =  mail.date
+          message_id = mail.message_id.to_s(16)
+          params[:message_id] = message_id 
+          params[:url_gmail] = "https://mail.google.com/mail/u/#{gmail.username}/#inbox/#{message_id}"
+
           res.push(params)
         end
         gmail.logout
